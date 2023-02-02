c5aba45f74518e86b6a98ec3a26be0df8f045919
NunezAN <alex101nunez@gmail.com>
2023-02-02 13:33:38 -0600

    fetching api and showing results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+function getAutocompleteResults(query) {
+  const fruits = [
+    "Apple",
+    "Banana",
+    "Kiwi",
+    "Orange",
+    "Strawberry",
+    "Watermelon",
+    "Mango",
+    "Grape",
+    "Lemon",
+    "Lime",
+    "Peach",
+    "Pear",
+    "Raspberry",
+    "Blueberry",
+    "Blackberry",
+    "Cherry",
+    "Coconut",
+    "Cranberry",
+    "Cucumber",
+    "Grapefruit",
+    "Honeydew",
+    "Jackfruit",
+    "Mandarin",
+    "Mango",
+    "Melon",
+    "Nectarine",
+    "Papaya",
+    "Pineapple",
+    "Plum",
+    "Pomegranate",
+  ];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        fruits.filter((fruit) =>
+          fruit.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }, Math.random() * 1000);
+  });
+}
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const fetchResults = async () => {
+      const fetchedData = await getAutocompleteResults(search);
+      setResults(fetchedData);
+    };
+    fetchResults();
+  }, [search]);
+  console.log(results);
+  // console.log(getAutocompleteResults("mango"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col items-center bg-gray-900">
+      <input
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        className="mt-24 mb-4"
+      />
+      <div className="text-gray-200 flex flex-col gap-2 items-start">
+        {results.map((result) => (
+          <div>{result}</div>
+        ))}
+      </div>
     </div>
   );
 }
